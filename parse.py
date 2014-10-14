9d04d72576acb69a106325eebe8ffe8b16b1959e
Jared Kerim <jared.l.kerim@gmail.com>
2014-10-14 20:52:04 +0000

    Add ITU operators (#14)
    - Add raw CSV data from ITU PDF (parsed using Tabula)
    - Update parser to incorporate both Wikipedia and ITU data
    - Include parsed/merged JSON operator data
    - Update countries as necessary

--- parse.py
+++ parse.py
@@ -1,19 +1,65 @@
 # A script that parses the wikipedia page into JSON.
-# run wget wget http://en.wikipedia.org/wiki/List_of_mobile_country_codes
+# run wget http://en.wikipedia.org/wiki/List_of_mobile_country_codes
 # then this..
+import json
+import os
+
 from BeautifulSoup import BeautifulSoup
 
-html = open('List_of_mobile_country_codes', 'r')
+from mobile_codes import Operator
 
-soup = BeautifulSoup(html)
 
-data = []
+def parse_wikipedia():
+    with open('List_of_mobile_country_codes', 'r') as htmlfile:
+        soup = BeautifulSoup(htmlfile)
+        operators = []
 
-for table in soup.findAll('table', attrs={'class': 'wikitable'}):
-    for row in table.findAll('tr'):
-        mcc, mnc, brand, operator = row.findChildren()[:4]
-        if mcc.text in ['MCC', '']:
-            continue
-        print u'        Operator("{0}", "{1}", "{2}", u"{3}"),'.format(
-            mcc.text, mnc.text, brand.text, operator.text)
+        for table in soup.findAll('table', attrs={'class': 'wikitable'}):
+            for row in table.findAll('tr'):
+                mcc, mnc, brand, operator = row.findChildren()[:4]
+                if mcc.text in ['MCC', '']:
+                    continue
 
+                operators.append(
+                    Operator(
+                        operator=operator.text, brand=brand.text,
+                        mcc=mcc.text, mnc=mnc.text))
+
+        return operators
+
+
+def parse_itu():
+    with open(os.path.join('source_data', 'itu.json'), 'rb') as jsonfile:
+        data = json.loads(jsonfile.read().decode())
+        operators = []
+
+        for line in data:
+            operators.append(Operator(*line))
+
+        return operators
+
+
+def merge_wiki_itu():
+    wiki_operators = parse_wikipedia()
+    itu_operators = parse_itu()
+    merged_operators = {}
+
+    for operator in wiki_operators:
+        operator_key = operator.mcc, operator.mnc
+        merged_operators[operator_key] = operator
+
+    for operator in itu_operators:
+        operator_key = operator.mcc, operator.mnc
+        merged_operators[operator_key] = operator
+
+    return merged_operators.values()
+
+
+def write_operators(operators):
+    with open(os.path.join('mobile_codes', 'json', 'operators.json'),
+              'wb') as outfile:
+        outfile.write(json.dumps(operators))
+
+
+if __name__ == '__main__':
+    write_operators(merge_wiki_itu())
